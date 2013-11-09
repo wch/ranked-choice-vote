1a925d7c25570a528674f30fb6f56e5169739c69
Winston Chang <winston@stdout.org>
2013-11-08 20:44:15 -0600

    Generalize to any number of votes

--- calculate.R
+++ calculate.R
@@ -3,22 +3,15 @@ wrap <- function(..., sep = "", width = 80) {
 }
 
 calculate_ranked_choice <- function(dat, round = 1) {
-  # Replace any empty entries with NA
-  dat$V1[dat$V1 == ""] <- NA
-  dat$V2[dat$V2 == ""] <- NA
-  dat$V3[dat$V3 == ""] <- NA
-
   # Get names of all candidates
-  all_names <- unique(c(as.character(dat$V1),
-                        as.character(dat$V2),
-                        as.character(dat$V3)))
+  all_names <- unique(as.vector(dat))
   all_names <- all_names[!is.na(all_names)]
-  dat$V1 <- factor(dat$V1, levels = all_names)
-  dat$V2 <- factor(dat$V2, levels = all_names)
-  dat$V3 <- factor(dat$V3, levels = all_names)
-  
+
   # Get counts of first-rank votes for each candidate -------------------------
-  counts <- as.data.frame(table(dat$V1))
+  # Turn first col of dat into a factor, with all_names as levels. This is so
+  # table() will report entries with zero count.
+  rank1 <- factor(dat[, 1], levels = all_names)
+  counts <- as.data.frame(table(rank1))
   names(counts) <- c("name", "votes")
   # Sort by votes
   counts <- counts[order(counts$votes, decreasing = TRUE), ]
@@ -54,23 +47,34 @@ calculate_ranked_choice <- function(dat, round = 1) {
   cat("\n\n")
 
   # Remove all instances of the lowest vote-getters
-  dat$V1[which(dat$V1 %in% drop_names)] <- NA
-  dat$V2[which(dat$V2 %in% drop_names)] <- NA
-  dat$V3[which(dat$V3 %in% drop_names)] <- NA
+  dat[dat %in% drop_names] <- NA
 
-  # If any NA's in V1 or V2, shift names over from next column.
-  # If a voter's first-rank vote was removed, their second-rank vote is now
-  # treated as their first-rank vote.
-  na_idx <- is.na(dat$V2)
-  dat$V2[na_idx] <- dat$V3[na_idx]
-  dat$V3[na_idx] <- NA
-  na_idx <- is.na(dat$V1)
-  dat$V1[na_idx] <- dat$V2[na_idx]
+  dat <- shift_left_na(dat)
 
   # Recurse
   calculate_ranked_choice(dat, round + 1)
 }
 
+# For any NA's in a given column, shift names over from next column. For
+# example, if a voter's first-rank vote was removed, their second-rank vote is
+# now treated as their first-rank vote.
+shift_left_na <- function(dat) {
+  ncols <- ncol(dat)
+
+  for (i in 1:(ncols-1)) {
+    # Find NA's in current column
+    na_idx <- is.na(dat[, i])
+
+    # For any NA's, shift over values from all subsequent columns and fill last
+    # with NA.
+    for (j in i:(ncols-1)) {
+      dat[na_idx, j] <- dat[na_idx, j+1]
+    }
+    dat[na_idx, ncols] <- NA
+  }
+
+  dat
+}
 
 # Read in a CSV file, calculate winner, and write to file
 process_file <- function(filename, outfile = NULL) {
@@ -85,10 +89,14 @@ process_file <- function(filename, outfile = NULL) {
   }
 
   # Read data: Columns are named V1, V2, V3, corresponding to 1st, 2nd, 3rd vote
-  data <- suppressWarnings(read.csv(filename, header = FALSE,
+  dat <- suppressWarnings(read.csv(filename, header = FALSE,
     fill = TRUE, stringsAsFactors = FALSE))
 
-  calculate_ranked_choice(data)
+  # Convert to matrix and replace blanks with NA
+  dat <- as.matrix(dat)
+  dat[dat == ""] <- NA
+
+  calculate_ranked_choice(dat)
 }
 
 process_file("SampleData.csv")
